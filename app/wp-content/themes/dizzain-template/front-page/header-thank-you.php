38b12e51dca0cf9c159f4a77ed662cab8b0ab844
pashasema <dosurgeest@gmail.com>
2019-12-05 02:50:43 +0300

    add close svg instead of font

--- app/wp-content/themes/dizzain-template/front-page/header-thank-you.php
+++ app/wp-content/themes/dizzain-template/front-page/header-thank-you.php
@@ -9,7 +9,7 @@
 			<p class="all-titles openform__title">Request successfully sent.</p>
 
 			<p class="openform__text">Thank you for your interest in Olynk. We will contact you shortly.</p>
-			<button title="Close (Esc)" type="button" class="mfp-close">×</button>
+			<button title="Close (Esc)" type="button" class="mfp-close"><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="40px" height="40px" fill="#fff" viewBox="0 0 357 357" style="enable-background:new 0 0 357 357;" xml:space="preserve"><g><g id="close"><polygon points="357,35.7 321.3,0 178.5,142.8 35.7,0 0,35.7 142.8,178.5 0,321.3 35.7,357 178.5,214.2 321.3,357 357,321.3 214.2,178.5 "/></g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg></button>
 		</div><!-- .openform__container -->
 		
 	</div><!-- #thankyou -->
